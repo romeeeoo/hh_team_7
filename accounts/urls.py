7428182668e7a43bdbf91d544af77a550ac0e5e3
romeeeoo <tarassenko@me.com>
2023-04-11 17:03:54 +0300

    logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from accounts.views import RegisterView
+from accounts.views import RegisterView, logout_view
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("logout/", logout_view, name="logout"),
 ]
--- accounts/views/__init__.py
+++ accounts/views/__init__.py
@@ -1,1 +1,2 @@
 from .register import RegisterView
+from .logout import logout_view
