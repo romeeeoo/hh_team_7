c15d949079d849b79636f2302c06c6186fc3dbd8
romeeeoo <tarassenko@me.com>
2023-04-13 16:25:31 +0300

    dump with categories

--- webapp/models/caterogy.py
+++ webapp/models/caterogy.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(blank=False, null=False)
+    name = models.CharField(blank=False, null=False, max_length=200)
 
     class Meta:
         verbose_name = "Category"
